52dcc282bd0d2c5ec199a547cd66f1e5b5d85f1d
unclebconnor <bc.boxomail@gmail.com>
2017-10-04 18:17:57 -0700

    fixed issue with routing after creating a journal item

--- controllers/journal.js
+++ controllers/journal.js
@@ -4,25 +4,45 @@ var db = require('../models');
 var passport = require('../config/ppConfig');
 var isLoggedIn = require('../middleware/isLoggedIn');
 
-
 router.get('/', isLoggedIn, function(req, res) {
-  res.render('journal/journalList', {currentUser: req.user});
+	db.sessionLog.findAll({
+		where: {userId: req.user.dataValues.id},
+		order: '"date" DESC'
+	})
+	.then(function(sessionList){
+		res.render('journal/journalList', {
+			currentUser: req.user,
+			sessionList: sessionList
+		});
+	});
 });
+
 
 router.get('/editJournal', isLoggedIn, function(req, res) {
   res.render('journal/editJournal', {currentUser: req.user});
 });
 
-router.post('/editJournal', function(req, res) {
-  var newEntry = req.body;
-  db.sessionLog.create({
-	userId: newEntry.userId,
-	title: newEntry.journalEntryTitle,
-	date: newEntry.journalEntryDate,
-	notes: newEntry.journalEntryNotes,
-	status: newEntry.journalEntryStatus
+router.post('/editJournal', isLoggedIn, function(req, res) {
+  	var newEntry = req.body;
+  	db.sessionLog.create({
+		userId: newEntry.userId,
+		title: newEntry.journalEntryTitle,
+		date: newEntry.journalEntryDate,
+		notes: newEntry.journalEntryNotes,
+		status: newEntry.journalEntryStatus
+	})
+  	.then(function(){
+		db.sessionLog.findAll({
+			where: {userId: req.user.dataValues.id},
+			order: '"date" DESC'
+		})
+		.then(function(sessionList){
+			res.render('journal/journalList', {
+				currentUser: req.user,
+				sessionList: sessionList
+			});
+		});
 	});
-  console.log(req.body);
 });
 
 
